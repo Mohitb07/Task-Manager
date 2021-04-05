9c256daec5cdb8ef2c200b44efb576a8cb47cde2
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2021-04-05 21:31:34 +0530

    Read endpoints added

--- src/index.js
+++ src/index.js
@@ -11,21 +11,55 @@ app.use(express.json())
 app.post('/users', (req, res)=>{
     const user = new User(req.body)
     user.save().then(()=>{
-        res.status(201).send(user)
+        res.sendStatus(201).send(user)
     }).catch((error)=>{
-        res.status(400).send(error)
+        res.sendStatus(400).send(error)
+    })
+})
+
+app.get('/users', (req, res)=> {
+    User.find({}).then(users=>{
+        res.send(users)
+    }).catch(error=>{
+        res.sendStatus(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res)=>{
+    const _id = req.params.id
+    User.findById(_id).then(user=>{
+        
+        res.send(user)
+    }).catch(error=>{
+        res.sendStatus(404).send()
     })
 })
 
 app.post('/tasks',(req, res)=>{
     const task = new Task(req.body)
     task.save().then(()=>{
-        res.status(201).send(task)
+        res.sendStatus(201).send(task)
     }).catch((error)=>{
-        res.status(400).send(error)
+        res.sendStatus(400).send(error)
     })
 })
 
+app.get('/tasks',(req, res)=>{
+    Task.find({}).then(task=>{
+        res.send(task)
+    }).catch(error=>{
+        res.sendStatus(404).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res)=>{
+    const _id = req.params.id
+    Task.findById(_id).then(task=>{
+        res.send(task)
+    }).catch(error=>{
+        res.sendStatus(404).send()
+    })
+})
 
 app.listen(port, ()=>{
     console.log('Server is up on port', port)
