3e2a2fc80f5ccac59f9ed7f5a863f59692325708
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2021-04-07 21:11:29 +0530

    hashed password (brcrypt) & login endpoint added

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,15 @@ const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
 
+router.post('/users/login', async(req, res)=>{
+    try{
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    }catch(error){
+        res.status(400).send(error)
+    }
+})
+
 router.post('/users', async (req, res)=>{
     const user = new User(req.body)
 
@@ -59,8 +68,16 @@ router.patch('/users/:id', async (req, res, next)=>{
         return res.status(400).send({ 'error': 'Invalid Updates'})
     }
     
-    try{       
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true})
+    try{   
+        const user = await User.findById(req.params.id)
+        updates.forEach((update)=> user[update] = req.body[update])
+        await user.save()    
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true})
+
+        if(!user) {
+            return res.status(404).send()
+        }
+
         res.send(user)
      }catch(error){
        res.status(500).send(error)
